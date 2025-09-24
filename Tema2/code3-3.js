421eb39f1efe5a738d0ba70c39bd31dc1490165e
Ruben <r.ternero.molina@iescristobaldemonroy.es>
2025-09-24 18:41:21 +0200

    new file:   Tema2/Ejercicio3.3.html     new file:   Tema2/code3-3.js

---
+++ Tema2/code3-3.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("Pedimos la altura y la base  de un triangulo");
+
+const altura = prompt('Introduce la altura del circulo', '0');
+
+const base = prompt('Introduce la base del circulo', '0');
+
+const area = base * altura/2
+
+console.log(area);
