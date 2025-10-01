90934eeba3cbc88cc08766cac549cc5a90c15e80
Ruben <r.ternero.molina@iescristobaldemonroy.es>
2025-10-01 20:18:03 +0200

    modified:   Tema3/ejercicio8.js

--- Tema3/ejercicio8.js
+++ Tema3/ejercicio8.js
@@ -1,14 +1,13 @@
 "use strict"
 
 /**
- * Reemplaza caracteres de un texto según dos cadenas de referencia.
  * 
  * @param {String} texto - Texto original.
  * @param {String} textoAReemplazar - Caracteres que serán reemplazados.
  * @param {String} textoAnterior - Caracteres por los cuales se reemplazará.
  */
 function reemplaza(texto, textoAReemplazar, textoAnterior) {
-    // Limpiar espacios y hacer el texto modificable
+    
     let textoLimpio = texto.replaceAll(" ", "").trim();
 
     // Validar que las longitudes coincidan
@@ -17,12 +16,12 @@ function reemplaza(texto, textoAReemplazar, textoAnterior) {
         return;
     }
 
-    // Reemplazar cada letra según correspondencia
+
     for (let i = 0; i < textoAReemplazar.length; i++) {
         const letraAReemplazar = textoAReemplazar[i];
         const letraAnterior = textoAnterior[i];
 
-        // Reemplazar todas las ocurrencias de la letra
+       
         textoLimpio = textoLimpio.replaceAll(letraAReemplazar, letraAnterior);
     }
 
