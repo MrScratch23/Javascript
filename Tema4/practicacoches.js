73d26d360d0e171880ca969742c3f46899fafa50
Ruben <r.ternero.molina@iescristobaldemonroy.es>
2025-11-05 17:00:38 +0100

    practicacoches cambiado

--- Tema4/practicacoches.js
+++ Tema4/practicacoches.js
@@ -2,6 +2,22 @@
 
 // funciones
 let arrayVehiculos = [];
+// vehiculos de prueba
+// Vehículos de prueba
+const vehiculo1 = new Vehiculo("Toyota", "Corolla", 2023, 20000, true);
+arrayVehiculos.push(vehiculo1);
+
+const coche1 = new Coche("Honda", "Civic", 2022, 25000, true, 4, "1234XYZ", "Gasolina");
+arrayVehiculos.push(coche1);
+
+const coche2 = new Coche("Tesla", "Model 3", 2023, 35000, true, 4, "5678ABC", "Electrico");
+arrayVehiculos.push(coche2);
+
+const moto1 = new Motocicleta("Yamaha", "R1", 2021, 15000, true, "A123BCD", 600, "Deportiva");
+arrayVehiculos.push(moto1);
+
+const moto2 = new Motocicleta("Kawasaki", "Ninja 400", 2022, 9000, true, "B234CDE", 399, "Deportiva");
+arrayVehiculos.push(moto2);
 
 
 const añadirVehiculo = function() {
@@ -52,13 +68,42 @@ const listarVehiculos = function() {
   let sol = `<ul>`
 
   for (const vehiculo of arrayVehiculos) {
-    
+      sol += `<li>${vehiculo.marca}</li>`;
+      sol += `<li>${vehiculo.modelo}</li>`;
+      sol += `<li>${vehiculo.año}</li>`;
+      sol += `<li>${vehiculo.precio}</li>`;
+      sol += `<li>${vehiculo.stock}</li>`;
+      sol += `<li>${vehiculo.obtenerDescripcion()}</li>`;
   }
+
+  sol += `</ul>`;
+
+  lista.innerHTML = sol;
 
 
 };
 
 const venderVehiculo = function() {
+  const matriculaVenta = document.querySelector("#matriculaVenta").value;
+  const mensajeVenta = document.querySelector("#mensajeVenta");
+  let sol = "";
+
+  for (const vehiculo of arrayVehiculos) {
+    if (matriculaVenta === vehiculo.matricula) {
+      if (vehiculo.stock === true) {
+        vehiculo.stock === false;
+      } else if (vehiculo.stock === false) {
+        vehiculo.stock === true;
+      }
+      sol += "Venta de vehiculo confirmado.";
+    } else {
+      sol += "Error en la venta. Matriculo no encontrada.";
+    }
+  }
+
+  mensajeVenta.innerHTML = sol;
+
+
   return
 };
 
@@ -78,3 +123,5 @@ btnAñadir.addEventListener('click', añadirVehiculo);
 btnListarVehiculos.addEventListener('click', listarVehiculos);
 btnVender.addEventListener('click', venderVehiculo);
 btnCalcularImpuestos.addEventListener('click', calcularImpuestos);
+
+
