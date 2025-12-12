59d1fcdefa78159ebce838c4416714466e938008
Ruben <r.ternero.molina@iescristobaldemonroy.es>
2025-12-12 21:11:43 +0100

    coomit

--- Tema7/Recursos-20251211/ejercicio4.js
+++ Tema7/Recursos-20251211/ejercicio4.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-fetch("https://mocktarget.apigee.net/")
+fetch("https://mocktarget.apigee.net/xml")
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => console.log(data));
